43a9cc0bae7316a82fb4737cdf7e6346b6099d13
Abhishek Singh Rajawat <abhisheksinghas805994@gmail.com>
2026-07-23 12:46:58 +0530

    Add solution for valid-perfect-square

---
+++ valid-perfect-square/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num)
+    {
+        
+        long long lo=0;
+        long long hi=num;
+         
+
+        while(lo<=hi)
+        {
+            long long mid=lo+(hi-lo)/2;
+            if(mid*mid==num){
+            
+                return true;
+            }
+
+            else if(mid*mid>num)
+            {
+                hi=mid-1;
+            }
+            else  lo=mid+1;
+            
+        }
+              
+                return false;
+              
+    }
+};
