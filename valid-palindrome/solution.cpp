d730fb6c0111a89644342020084d7748bc987610
Abhishek Singh Rajawat <abhisheksinghas805994@gmail.com>
2026-07-23 12:46:30 +0530

    Add solution for valid-palindrome

---
+++ valid-palindrome/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+     bool isPalindrome(const std::string& s) {
+    int left = 0;
+    int right = s.length() - 1;
+
+    while (left < right) {
+        
+        while (left < right && !std::isalnum(s[left])) {
+            ++left;
+        }
+         
+        while (left < right && !std::isalnum(s[right])) {
+            --right;
+        }
+
+         
+        if (std::tolower(s[left]) != std::tolower(s[right])) {
+            return false;
+        }
+
+        ++left;
+        --right;
+    }
+
+    return true;
+}
+
+};
